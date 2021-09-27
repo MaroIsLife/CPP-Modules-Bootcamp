3b841d6768d4d67283115015d56525ebee747d38
Marouane Ougnou <mougnou@s2r4p3.1337.ma>
2021-09-27 17:26:27 +0100

    Module 00 ex00

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctype.h>
+
+int main(int ac, char **av)
+{
+    int i;
+    int b;
+
+    i = 1;
+    while (av[i] != NULL)
+    {
+        b = 0;
+        while (av[i][b] != '\0')
+        {
+            std::cout << (char)toupper(av[i][b]);
+            b++;
+        }
+        i++;
+        if (av[i] == NULL)
+            break ;
+        std::cout << " ";
+    }
+    std::cout << "\n";
+    return (0);
+}
