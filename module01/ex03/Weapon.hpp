80e060bc6efa4dc53d260f711c274b5eecfd39f7
Marouane Ougnou <mougnou@s2r4p4.1337.ma>
2021-10-04 20:27:51 +0100

    Pushing

---
+++ module01/ex03/Weapon.hpp
@@ -0,0 +1,14 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+class Weapon
+{
+private:
+	std::string type;
+
+public:
+	std::string const	&getType();
+	void				setType(std::string type);
+};
+
+#endif
