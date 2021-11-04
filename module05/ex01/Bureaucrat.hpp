2834b999682c4d5297dd71bf663a26c6724b3be1
Marouane Ougnou <mougnou@e1r12p10.1337.ma>
2021-11-04 21:31:44 +0100

    Pushing

---
+++ module05/ex01/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+private:
+	const std::string	_name;
+	int					_grade;	
+public:
+	Bureaucrat();
+	~Bureaucrat();
+	Bureaucrat(Bureaucrat &p);
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat	&operator=(Bureaucrat &ref);
+	std::string getName();
+	int			getGrade();
+	void		incrementGrade();
+	void 		decrementGrade();
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			const char *what() const throw();
+	};
+	//Overriding What() from std::exception class
+};
+std::ostream &operator<<(std::ostream &obj, Bureaucrat &abc);
+
+#endif
---
+++ module05/ex01/Form.hpp
@@ -0,0 +1,16 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+
+class Form
+{
+private:
+	const std::string	_name;
+	bool 				_sign;
+	const int 			_grade_sign;
+	const int			_grade_execute;
+public:
+	
+};
+
+#endif
